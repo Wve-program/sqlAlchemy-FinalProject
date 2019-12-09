69c09608a46b95a5567690e772f07feed76db81d
Wve-program <55114757+Wve-program@users.noreply.github.com>
2019-12-09 15:38:05 -0500

    Fixed: Adjusted File and Directory

--- Assignment/Homework.py
+++ Assignment/Homework.py
@@ -1,10 +1,13 @@
 #This is the tutorial code used for the Homework and tested separately between here and pycharm
-
+#Step 1
 from sqlalchemy.orm import sessionmaker, Session
 Session = sessionmaker(bind=engine)
 
 session = Session()
 
+#End of Step 1
+
+#Step 2
 c1 = Customer(first_name = 'Jane', 
               last_name = 'Doe', 
               username = 'JDoe', 
@@ -22,3 +25,22 @@ c1, c2
 
 c1.first_name, c1.last_name
 c2.first_name, c2.last_name
+
+#Step 2 continuing, added objects, but now to call them.
+
+session.add(c1)
+session.add(c2)
+
+#adding changes and objects to the session
+
+session.add_all([c1, c2])
+session.commit()
+
+#End of Step 2, The point of step two is defining the data put in the session.
+#Using c1.id can call and display an output message of what C1 is defined as. 
+
+#Step 3, Query Data
+
+session.query(Customer).all()
+#Output: Displays Jane and John Doe's name
+
